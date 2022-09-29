dcb660b2aeb408e600250624174be53fd4dcd989
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2022-09-28 23:21:11 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,38 +17,7 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --SELECT SUM(c12) FROM tbl1.csv
-import sqlite3
+import pandas as pd
 
-conn = sqlite3.connect(":memory:")
-cur = conn.cursor()
-conn.executescript(
-    """
-DROP TABLE IF EXISTS tbl1;
-
-CREATE TABLE tbl1 (
-    K0  CHAR(1),
-    K1  INT,
-    c12 FLOAT,
-    c13 INT,
-    c14 DATE,
-    c15 FLOAT,
-    c16 CHAR(4)
-    );
-"""
-)
-conn.commit()
-
-with open("tbl1.csv", "rt") as f:
-    data = f.readlines()
-
-data
-
-# Separa los campos por comas
-data = [line.split(",") for line in data]
-
-# Convierte la fila en una tupla
-data = [tuple(line) for line in data]
-
-cur.executemany("INSERT INTO tbl1 VALUES (?,?,?,?,?,?,?)", data)
-
-cur.execute("SELECT SUM(c12) FROM tbl1").fetchall()[0][0]
+archivo = pd.read_csv("tbl1.csv", sep = ",", header = None)
+archivo[2].sum()
