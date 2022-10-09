a16561a85727e934c3781933dc093006a36aaae9
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2022-10-08 22:06:09 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -18,32 +18,55 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --SELECT SUM(c12) FROM tbl1.csv
 
-import sqlite3
-import pandas as pd
+import doctest
+import subprocess
 
-conn = sqlite3.connect(":memory:")
-cur = conn.cursor()
-conn.executescript(
-    """
-DROP TABLE IF EXISTS tbl1;
+def run_answer():
+    '''Codigo especifico para ejecutar la respuesta'''
+    #----------------------------------------------------------------------------------------------
+    # Prepara los datos para evaluar la tarea
+    #----------------------------------------------------------------------------------------------
+    import sqlite3
+    conn = sqlite3.connect(':memory:')
+    cur = conn.cursor()
+    sqlcmd = """
+        CREATE TABLE tbl (
+            K0  CHAR(1),
+            K1  INT,
+            c12 FLOAT,
+            c13 INT,
+            c14 DATE,
+            c15 FLOAT,
+            c16 CHAR(4)
+            );"""
+    cur.execute(sqlcmd).fetchall()
 
-CREATE TABLE tbl1 (
-    K0  CHAR(1),
-    K1  INT,
-    c12 FLOAT,
-    c13 INT,
-    c14 DATE,
-    c15 FLOAT,
-    c16 CHAR(4)
-    );
-"""
-)
-conn.commit()
+    ## Carga los datos y los inserta en la tabla
+    text = open('data.csv', 'rt', encoding='utf-8').readlines()
+    text = [line[:-1] if line[-1] == '\n' else line for line in text]
+    text = [line.split(',') for line in text]
+    text = [tuple(line) for line in text]
+    cur.executemany('INSERT INTO tbl VALUES (?,?,?,?,?,?,?)', text)
 
-data = pd.read_csv("https://raw.githubusercontent.com/ciencia-de-los-datos/programacion-usando-sqlite3-DiegoAlexUNALMED/main/tbl1.csv", sep = ",", header = None)
-data.rename(columns = {0:"K0", 1:"K1",2:"c12",3:"c13",4:"c14",5:"c15",6:"c16"}, inplace = True)
+    #----------------------------------------------------------------------------------------------
+    # Ejecuta el código del estudiante
+    #----------------------------------------------------------------------------------------------
+    import pandas as pd
+    answer = open('question.sql', 'rt', encoding='utf-8').readlines()
+    answer = [row for row in  answer if len(row) >= 2 and row[0:2] != '--']
+    answer = ''.join(answer)
+    if answer.strip() == '':
+        return None
+    else:
+        return pd.read_sql_query(answer, conn)
 
-cur.executemany("INSERT INTO tbl1 VALUES (?,?,?,?,?,?,?)", data.values)
-
-df = pd.DataFrame({'SUM(c12)':[cur.execute("SELECT SUM(c12) FROM tbl1").fetchall()[0][0]]})
-print(df)
+#--------------------------------------------------------------------------------------------------
+# Grader (generic)
+#--------------------------------------------------------------------------------------------------
+subprocess.run(['rm', '-f', '_SUCCESS']) # borra el flag de exito de la corrida
+RESULT = doctest.testmod()               # ejecuta el doctest
+FAIL, _ = RESULT                         # fail, total
+if FAIL == 0:                            # grading
+    open('_SUCCESS', 'a').close()        #
+else:
+    print('\n')
