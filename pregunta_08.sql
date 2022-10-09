31fea306f38fea9259bff79458dcc206ed28b5e1
DiegoAlexUNALMED <112149672+DiegoAlexUNALMED@users.noreply.github.com>
2022-10-08 23:24:58 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,4 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-select c23 as YEAR, avg(c21) from tbl2 group by c23 order by c23
+select SUBSTR(c23,1,4) as YEAR, avg(c21) from tbl2 group by SUBSTR(c23,1,4)
